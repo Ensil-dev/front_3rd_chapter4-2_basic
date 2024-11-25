bb32967e21d9c6d34e13c4badc5c64186d3c49f4
Ensil-dev <lasags@naver.com>
2024-11-25 18:30:43 +0900

    chore: vite 도입

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+    root: '.',
+    publicDir: 'images', // 정적 assets
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets'
+    },
+    server: {
+      port: 3000,
+      open: true
+    }
+  }
